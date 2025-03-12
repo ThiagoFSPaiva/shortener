3253f9c56ddd76e89a9fcfa5014e1156e6f3b223
Thiago <thiagofellipe151@gmail.com>
2025-03-12 20:55:07 -0300

    Initial commit

---
+++ src/test/java/br/com/thiagofspaiva/shortener/ShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiagofspaiva.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
